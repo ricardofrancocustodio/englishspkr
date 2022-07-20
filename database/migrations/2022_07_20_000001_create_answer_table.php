3c24e7fae733e55f62d30a3e048670382623d1b2
ricardofrancocustodio <56692371+ricardofrancocustodio@users.noreply.github.com>
2022-07-20 09:30:57 -0300

    migrations, auth implementation, register link on login page

---
+++ database/migrations/2022_07_20_000001_create_answer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->id('id_answer');
+            $table->string('id_question');
+            $table->string('answer_path');
+            $table->string('answer_file_name');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer');
+    }
+};
---
+++ database/migrations/2022_07_20_000001_create_question_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->id('id_question');
+            $table->string('question_text');
+            $table->string('img_question');
+            $table->string('audio_question');
+            $table->string('id_time_question');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+};
